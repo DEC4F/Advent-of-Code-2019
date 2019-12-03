522b952650635835b6ec1a777fa73934b991e54d
decaf <rarunok@yahoo.com>
2019-12-03 14:20:31 -0600

    :sparkles: updated day 1 with printf and input for day 2

--- Day1/CalcFuel.ml
+++ Day1/CalcFuel.ml
@@ -7,5 +7,6 @@ let rec calc_fuel_2 n =
   if sum <= 0 then 0 else sum + calc_fuel_2 sum
 
 let day_1 (fpath : string) (f : int -> int) =
-  In_channel.read_lines fpath
-  |> List.fold_left ~init:0 ~f:(fun acc ele -> acc + f (Int.of_string ele)
+  let ans = In_channel.read_lines fpath
+            |> List.fold_left ~init:0 ~f:(fun acc ele -> acc + f (Int.of_string ele)) in
+  Printf.printf "Answer = %d\n" ans;
