da7c7b748a9e53c496a821f194462430ca94eb01
decaf <rarunok@yahoo.com>
2019-12-04 22:37:23 -0600

    added solution for day 2, with reference from https://github.com/gwaerondor/aoc2019

--- Day4/PW.ml
+++ Day4/PW.ml
@@ -9,24 +9,35 @@ let num_to_list n =
   |> String.to_list
   |> List.map ~f:(fun x -> int_of_char x - 48)
 
-let rec same_adjacent = function
-  | [] -> false
-  | [x] -> false
-  | x1 :: x2 :: xs when x1 = x2 -> true
-  |  _ :: x2 :: xs -> same_adjacent (x2 :: xs)
-
 let rec non_dec = function
   | [] -> true
-  | [x] -> true
+  | [_] -> true
   | x1 :: x2 :: xs when x1 > x2 -> false
   |  _ :: x2 :: xs -> non_dec (x2 :: xs)
 
-let part_1 lo hi =
+let rec same_adjacent = function
+  | [] -> false
+  | [_] -> false
+  | x1 :: x2 :: xs when x1 = x2 -> true
+  |  _ :: x2 :: xs -> same_adjacent (x2 :: xs)
+
+let rec same_adjacent_2 = function
+  | [] -> false
+  | [_] -> false
+  | x :: xs -> let same x' = x' = x  in
+               if List.take_while ~f:same xs
+                  |> List.length
+                  |> (=) 1
+               then true
+               else same_adjacent_2 (List.drop_while ~f:same xs)
+
+let part lo hi rule =
   List.init (hi - lo + 1) (fun x -> x + lo)
   |> List.map ~f:num_to_list
-  |> List.filter ~f:same_adjacent
   |> List.filter ~f:non_dec
+  |> List.filter ~f:rule
   |> List.length
 
 let run () =
-  Printf.printf "Answer for Part 1 = %d" (part_1 lo hi)
+  Printf.printf "Answer for Part 1 = %d\n" (part lo hi same_adjacent);
+  Printf.printf "Answer for Part 2 = %d\n" (part lo hi same_adjacent_2)
